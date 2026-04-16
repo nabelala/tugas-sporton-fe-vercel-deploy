e738353f7cef5bc90f82ff5ec8846ab2773d29e9
Nabila Satyayana Parluhutan <nabilasatyansp@gmail.com>
2026-04-16 21:35:11 +0700

    tugas live session 1

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,14 @@
+import CategoriesSection from "./components/Home/categories";
+import HeroSection from "./components/Home/hero";
+import ProductsSection from "./components/Home/products";
+
+
+export default function Home() {
+  return (
+  <main>
+<HeroSection />
+<CategoriesSection />
+<ProductsSection />
+  </main>
+  );
+}
